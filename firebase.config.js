1356586b5e7082ba54726b26a841c49702b2b934
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-03 11:35:42 +0200

    Add files via upload

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIej7yNj0LkkLd6VtQxBL4mEDSsHLJvig",
+  authDomain: "pet-health-tracker-7164d.firebaseapp.com",
+  projectId: "pet-health-tracker-7164d",
+  storageBucket: "pet-health-tracker-7164d.firebasestorage.app",
+  messagingSenderId: "251170885789",
+  appId: "1:251170885789:web:2c16a20f96da9f6a960474",
+  measurementId: "G-GKD3RVNVLV"
