b2f7d1ea1442dc6278ea00ae1d11aeb58db7ed2c
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-07 23:08:35 +0200

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -3,6 +3,8 @@
     {
       "path": "/api/send-reminders",
       ""schedule": "0 6,14 * * *" // 9AM & 5PM local time (UTC+3)
+      "headers": {
+        "Authorization": "Bearer process.env.CRON_SECRET"
     }
   ]
 }
