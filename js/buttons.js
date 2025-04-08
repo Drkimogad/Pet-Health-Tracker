9d3473c52161a597c2abc18305926f7e12a59038
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-08 18:41:35 +0200

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,5 @@
-import { editingProfileIndex, loadSavedPetProfile } from './js/profiles.js';
-import { deleteOverdueReminder } from './js/reminders-validation.js';
+import { editingProfileIndex, loadSavedPetProfile } from '.js/profiles.js';
+import { deleteOverdueReminder } from '.js/reminders-validation.js';
 
 //* FUNCTION TO ATTACHED BUTTONS TO SAVED PROFILES *//
 const savedProfilesList = document.getElementById('savedProfilesList');
