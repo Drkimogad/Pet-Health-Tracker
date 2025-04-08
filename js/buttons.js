af99adff4c367e0220ef08967760958d2af56878
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-08 15:42:51 +0200

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,5 @@
 import { editingProfileIndex, loadSavedPetProfile, deletePetProfile, sharePetProfile, printPetProfile, editPetProfile, generateQRCode } from './js/profiles.js';
-import { deleteOverdueReminder } from './js/reminders-validation.js';
+import { deleteOverdueReminder } from 'https://drkimogad.github.io/Pet-Health-Tracker/js/reminders-validation.js';
 
 
 //* FUNCTION TO ATTACHED BUTTONS TO SAVED PROFILES *//
