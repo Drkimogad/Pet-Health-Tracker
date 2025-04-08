1cd8496fba18735d04a7b62459e71ad16283e532
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-08 18:41:54 +0200

    Update initialization.js

--- js/initialization.js
+++ js/initialization.js
@@ -2,8 +2,8 @@
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
-import { setupNotifications } from './js/pushNotifications.js';
-import { loadSavedPetProfile } from './js/profiles.js';
+import { setupNotifications } from '.js/pushNotifications.js';
+import { loadSavedPetProfile } from '.js/profiles.js';
 
 //* FIREBASE IMPORT AND INITIALIZATION *//
 firebase.initializeApp(firebaseConfig);
