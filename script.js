ff8db1699673704d6c26e7889817583c0b864340
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-28 17:20:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1190,7 +1190,7 @@ Emergency Contact: ${emergencyContact.name || 'N/A'} (${emergencyContact.relatio
     } finally {
       const loader = qrWindow.document.querySelector('.loader');
       if (loader) loader.style.display = 'none';
-      });
+      }
   catch (error) { // <- Add catch directly after closing try brace
     console.error("Main Error:", error);
     alert("QR initialization failed");
