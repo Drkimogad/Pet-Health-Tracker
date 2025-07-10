0a36508371f77be8456ad2f740dabd7980eddce2
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-07-10 17:39:58 +0200

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -281,14 +281,14 @@ async function loadSavedPetProfile() {
     }
 
     savedProfiles.forEach((profile, index) => {
-  // Emergency contacts NA fixed
-  // const emergencyContact = profile.emergencyContacts?.[0] || {};   old
-      const emergencyContact = (
-    Array.isArray(profile.emergencyContacts) && profile.emergencyContacts.length > 0
-   )
-     ? profile.emergencyContacts[0]
-     : { name: '', phone: '', relationship: '' };
-     console.log('✅ Emergency Contact:', emergencyContact);
+  // Emergency contacts NA
+  const emergencyContact = profile.emergencyContacts?.[0] || {};  // old
+  //  const emergencyContact = (
+  // Array.isArray(profile.emergencyContacts) && profile.emergencyContacts.length > 0
+  //   )
+  //   ? profile.emergencyContacts[0]
+  // : { name: '', phone: '', relationship: '' };
+  //   console.log('✅ Emergency Contact:', emergencyContact);
 
 
       const petCard = document.createElement('li');
