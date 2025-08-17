40a50d8c6b381dc03669019425af75b5ae458568
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-08-17 23:16:03 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,9 @@ import cloudinary from "cloudinary";
 
 // Configure Cloudinary using environment variables
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
-  api_key: process.env.CLOUDINARY_API_KEY || "",
-  api_secret: process.env.CLOUDINARY_API_SECRET || ""
+  cloud_name: functions.config().cloudinary.cloud_name || "",
+  api_key: functions.config().cloudinary.api_key || "",
+  api_secret: functions.config().cloudinary.api_secret || ""
 });
 
 // Test connectivity to Cloudinary
