9a6ac37862364e61c6b00358a76a126aa79dd416
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-10-06 15:31:56 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -486,18 +486,6 @@ async function removeOfflineProfile(db, id) {
 }
 
 
-//==========================================
-// age calculation utility
-//==================================
-function calculateAge(birthday) {
-  const birthDate = new Date(birthday);
-  const today = new Date();
-  let age = today.getFullYear() - birthDate.getFullYear();
-  const monthDiff = today.getMonth() - birthDate.getMonth();
-  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
-  }
-  return age;
-}
+
 
 
