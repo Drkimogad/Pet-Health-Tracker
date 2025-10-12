02b8eda30969e43bcdd79452411c742b378618c6
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-10-12 22:01:55 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -21,22 +21,22 @@ window.showErrorToUser = showErrorToUser;
 
 
 // 🔶 State Management🔶🔶🔶
-//const VALID_ORIGINS = [
-//  'https://drkimogad.github.io',
-//  'https://drkimogad.github.io/Pet-Health-Tracker',
-//  'https://pet-health-tracker-4ec31.web.app' // ADD THIS
-//];
-// Runtime origin check
-//if (!VALID_ORIGINS.includes(window.location.origin)) {
-//  window.location.href = 'https://drkimogad.github.io/Pet-Health-Tracker';
-//}
-
 const VALID_ORIGINS = [
   'https://drkimogad.github.io',
   'https://drkimogad.github.io/Pet-Health-Tracker',
-  'https://pet-health-tracker-4ec31.web.app',
-  'https://pet-health-tracker-4ec31.firebaseapp.com'
+  'https://pet-health-tracker-4ec31.web.app' // ADD THIS
 ];
+ Runtime origin check
+if (!VALID_ORIGINS.includes(window.location.origin)) {
+  window.location.href = 'https://drkimogad.github.io/Pet-Health-Tracker';
+}
+
+//const VALID_ORIGINS = [
+//  'https://drkimogad.github.io',
+//  'https://drkimogad.github.io/Pet-Health-Tracker',
+//  'https://pet-health-tracker-4ec31.web.app',
+//  'https://pet-health-tracker-4ec31.firebaseapp.com'
+//];
 
 
 
